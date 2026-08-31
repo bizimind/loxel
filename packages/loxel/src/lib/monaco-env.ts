b5fdfd94c213f0c0666f8981b380365265e22692
Ori Shalom <ori.100000@gmail.com>
2026-08-31 12:25:40 +0300

    chore(deps): update Monaco Editor to v0.56

--- packages/loxel/src/lib/monaco-env.ts
+++ packages/loxel/src/lib/monaco-env.ts
@@ -1,9 +1,9 @@
 import { shikiToMonaco } from "@shikijs/monaco";
 import * as monaco from "monaco-editor";
-import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker.js?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker.js?worker";
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker.js?worker";
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker.js?worker";
 
 import { useWorktreeStore } from "@/store/worktrees";
 
