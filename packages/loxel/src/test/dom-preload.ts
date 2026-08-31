ef4e29fdce8bd54c5bddb84b9d39b74afdd03e96
Ori Shalom <ori-shalom@users.noreply.github.com>
2026-08-31 13:22:37 +0300

    chore(loxel): update DOM test dependencies (#143)
    * chore(loxel): update DOM test dependencies
    
    * test: load DOM matchers after Happy DOM
    
    * chore: remove unused motion dependency

--- packages/loxel/src/test/dom-preload.ts
+++ packages/loxel/src/test/dom-preload.ts
@@ -1,9 +1,5 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
-import * as matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "bun:test";
 
 GlobalRegistrator.register();
 
 Element.prototype.scrollIntoView = function scrollIntoView() {};
-
-expect.extend(matchers);
